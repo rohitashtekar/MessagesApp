23a8abc8d5e23484c47f5608dbaa0d0f2bad2fac
Rohit Ashtekar <RAshtekar@ABS-CBN.COM>
2024-04-11 17:54:46 +0530

    ADDED: dtos

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto'
 
 @Controller('messages')
 export class MessagesController {
@@ -7,7 +8,7 @@ export class MessagesController {
 
     
     @Post()
-    createMessage(@Body() body: any) {
+    createMessage(@Body() body: CreateMessageDto) {
         console.log(body);
     }
 
